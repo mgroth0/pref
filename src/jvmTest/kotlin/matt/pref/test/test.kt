41a6a4ca861550e08500058ff14803eaa0846cc4
mgroth0 <mgroth49@gmail.com>
2023-08-07 01:16:07 -0400

    added src/jvmTest/kotlin/matt/pref/test/test.kt,changed build.json

---
+++ src/jvmTest/kotlin/matt/pref/test/test.kt
@@ -0,0 +1,16 @@
+package matt.pref.test
+
+
+import matt.pref.safepref.SafePref
+import matt.test.JupiterTestAssertions.assertRunsInOneMinute
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PrefTests {
+    @Test
+    fun setAndGetProperty() = assertRunsInOneMinute {
+        val prop = SafePref("matt.pref.test.pref")
+        prop.putInt("testInt", 3)
+        assertEquals(3, prop.getInt("testInt", 0))
+    }
+}
