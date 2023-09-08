5cf5416e063d4fc7e140c2a6fe5492331c4b3d4d
mgroth0 <mgroth49@gmail.com>
2023-09-07 22:21:19 -0400

    changed 3 files

--- src/jvmMain/kotlin/matt/pref/obs/obs.kt
+++ src/jvmMain/kotlin/matt/pref/obs/obs.kt
@@ -3,169 +3,213 @@ package matt.pref.obs
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
+import matt.async.thread.namedThread
 import matt.obs.MObservable
 import matt.obs.prop.BindableProperty
 import matt.pref.PrefNode
 import matt.pref.PrefNodeBase
 import java.util.prefs.Preferences
-import kotlin.concurrent.thread
 import kotlin.reflect.KProperty
 
+open class PrefDomain(val key: String) {
+    init {
+        require(!key.endsWith("."))
+    }
+
+    operator fun get(name: String) = PrefDomain("$key.$name")
+}
+
 abstract class ObsPrefNode(
-  key: String,
-  oldNames: List<String> = listOf(),
-  oldKeys: List<String> = listOf(),
-  json: Json = Json
-): PrefNodeBase() {
+    key: String,
+    oldNames: List<String> = listOf(),
+    oldKeys: List<String> = listOf(),
+    json: Json = Json
+) : PrefNodeBase() {
 
-  init {
-	thread(isDaemon = true) {
-	  oldNames.forEach {
-		Preferences.userRoot().node(it).apply {
-		  removeNode()
-		  flush()
-		}
-	  }
-	}
-  }
+    constructor(key: PrefDomain) : this(key.key)
 
-  protected inline fun <reified T: Any> obj(defaultValue: T? = null, silent: Boolean = false) =
-	ObjObsPrefProvider(T::class.serializer(), defaultValue, silent = silent)
+    init {
+        namedThread("ObsPrefNode init Thread", isDaemon = true) {
+            oldNames.forEach {
+                Preferences.userRoot().node(it).apply {
+                    removeNode()
+                    flush()
+                }
+            }
+        }
+    }
 
-  protected inline fun <reified T: MObservable> obsObj(noinline defaultValue: ()->T) =
-	ObsObjObsPrefProvider(T::class.serializer(), defaultValue)
+    protected inline fun <reified T : Any> obj(
+        defaultValue: T? = null,
+        silent: Boolean = false
+    ) = ObjObsPrefProvider(serializer<T>(), defaultValue, silent = silent)
 
-  override fun string(defaultValue: String?) = StringObsPrefProvider(defaultValue)
-  override fun int(defaultValue: Int?) = IntObsPrefProvider(defaultValue)
-  override fun bool(defaultValue: Boolean?) = BoolObsPrefProvider(defaultValue)
+//    protected fun <T : Any> objNoInline(
+//        cls: KClass<T>,
+//        defaultValue: T? = null,
+//        silent: Boolean = false,
+//    ) =
+//        ObjObsPrefProvider(cls.serializer(), defaultValue, silent = silent)
 
-  private val prefNode by lazy { PrefNode(key, oldKeys, json) }
+    protected inline fun <reified T : MObservable> obsObj(noinline defaultValue: () -> T) =
+        ObsObjObsPrefProvider(serializer<T>(), defaultValue)
 
-  private val prefs by lazy { mutableSetOf<ObsPref<*>>() }
+    override fun string(defaultValue: String?) = StringObsPrefProvider(defaultValue)
+    override fun int(defaultValue: Int?) = IntObsPrefProvider(defaultValue)
+    override fun bool(defaultValue: Boolean?) = BoolObsPrefProvider(defaultValue)
 
-  override fun delete() {
-	prefNode.delete()
-	prefs.forEach {
-	  it.reset()
-	}
-  }
+    private val prefNode by lazy { PrefNode(key, oldKeys, json) }
 
+    private val prefs by lazy { mutableSetOf<ObsPref<*>>() }
 
-  protected abstract inner class ObsPref<T> {
-	init {
-	  prefs += this
-	}
-
-	protected abstract var pref: T?
-	private val obsProp by lazy {
-	  BindableProperty(pref).apply {
-		onChange {
-		  pref = it
-		}
-	  }
-	}
-
-	fun reset() {
-	  obsProp.value = pref
-	}
-
-	operator fun getValue(
-	  thisRef: Any?, property: KProperty<*>
-	): BindableProperty<T?> = obsProp
-  }
-
-  protected inner class ObsObjObsPrefProvider<T: MObservable>(
-	val ser: KSerializer<T>,
-	private val defaultValue: ()->T
-  ) {
-	operator fun provideDelegate(
-	  thisRef: Any?, prop: KProperty<*>
-	) = ObsObjObsPref(ser, defaultValue, prop.name)
-  }
-
-  protected inner class ObsObjObsPref<T: MObservable>(ser: KSerializer<T>, defaultValue: ()->T, key: String) {
+    override fun delete() {
+        prefNode.delete()
+        prefs.forEach {
+            it.reset()
+        }
+    }
 
 
-	private val thePref = prefNode.ObsObjPref(ser, defaultValue, key)
-	private val obsObj by lazy {
-	  thePref.get().apply {
-		observe {
-		  println("saving ObsObjobsPref")
-		  save()
-		}
-	  }
-	}
+    protected abstract inner class ObsPref<T> {
+        init {
+            prefs += this
+        }
+
+        protected abstract var pref: T?
+        private val obsProp by lazy {
+            BindableProperty(pref).apply {
+                onChange {
+                    pref = it
+                }
+            }
+        }
+
+        fun reset() {
+            obsProp.value = pref
+        }
+
+        operator fun getValue(
+            thisRef: Any?,
+            property: KProperty<*>
+        ): BindableProperty<T?> = obsProp
+    }
+
+    protected inner class ObsObjObsPrefProvider<T : MObservable>(
+        val ser: KSerializer<T>,
+        private val defaultValue: () -> T
+    ) {
+        operator fun provideDelegate(
+            thisRef: Any?,
+            prop: KProperty<*>
+        ) = ObsObjObsPref(ser, defaultValue, prop.name)
+    }
+
+    protected inner class ObsObjObsPref<T : MObservable>(
+        ser: KSerializer<T>,
+        defaultValue: () -> T,
+        key: String
+    ) {
 
 
-	/*	private val obsObj by lazy {
-		  println("setting up pref $pref observer with key $key")
-		  pref.observe {
-			println("saving ObsObjobsPref")
-			save()
-		  }
-		  pref
-		}*/
-
-	/*init {
-
-	  obsObj
-	}*/
-
-	fun save() {
-	  thePref.putIntoNode(obsObj)
-	}
-
-	operator fun getValue(
-	  thisRef: Any?, property: KProperty<*>
-	): T {
-	  println("returning obsObj")
-	  return obsObj
-	}
-  }
+        private val thePref = prefNode.ObsObjPref(ser, defaultValue, key)
+        private val obsObj by lazy {
+            thePref.get().apply {
+                observe {
+                    println("saving ObsObjobsPref")
+                    save()
+                }
+            }
+        }
 
 
-  protected inner class ObjObsPrefProvider<T: Any>(
-	val ser: KSerializer<T>,
-	private val defaultValue: T? = null,
-	val silent: Boolean
-  ) {
-	operator fun provideDelegate(
-	  thisRef: Any?, prop: KProperty<*>
-	) = ObjObsPref(ser, defaultValue, prop.name, silent = silent)
-  }
+        /*	private val obsObj by lazy {
+              println("setting up pref $pref observer with key $key")
+              pref.observe {
+                println("saving ObsObjobsPref")
+                save()
+              }
+              pref
+            }*/
 
-  protected inner class ObjObsPref<T: Any>(ser: KSerializer<T>, defaultValue: T? = null, key: String, silent: Boolean):
-	  ObsPref<T>() {
-	override var pref by prefNode.ObjPref(ser, defaultValue, key, silent = silent)
-  }
+        /*init {
 
-  protected inner class StringObsPrefProvider(private val defaultValue: String? = null) {
-	operator fun provideDelegate(
-	  thisRef: Any?, prop: KProperty<*>
-	) = StringObsPref(defaultValue = defaultValue, prop.name)
-  }
+          obsObj
+        }*/
 
-  protected inner class StringObsPref(defaultValue: String? = null, key: String): ObsPref<String>() {
-	override var pref by prefNode.StringPref(defaultValue, key)
-  }
+        fun save() {
+            thePref.putIntoNode(obsObj)
+        }
 
-  protected inner class IntObsPrefProvider(private val defaultValue: Int? = null) {
-	operator fun provideDelegate(
-	  thisRef: Any?, prop: KProperty<*>
-	) = IntObsPref(defaultValue, prop.name)
-  }
+        operator fun getValue(
+            thisRef: Any?,
+            property: KProperty<*>
+        ): T {
+            println("returning obsObj")
+            return obsObj
+        }
+    }
 
-  protected inner class IntObsPref(defaultValue: Int? = null, key: String): ObsPref<Int>() {
-	override var pref by prefNode.IntPref(defaultValue, key)
-  }
 
-  protected inner class BoolObsPrefProvider(private val defaultValue: Boolean? = null) {
-	operator fun provideDelegate(
-	  thisRef: Any?, prop: KProperty<*>
-	) = BoolObsPref(defaultValue, prop.name)
-  }
+    protected inner class ObjObsPrefProvider<T : Any>(
+        val ser: KSerializer<T>,
+        private val defaultValue: T? = null,
+        val silent: Boolean
+    ) {
+        operator fun provideDelegate(
+            thisRef: Any?,
+            prop: KProperty<*>
+        ) = ObjObsPref(ser, defaultValue, prop.name, silent = silent)
+    }
 
-  protected inner class BoolObsPref(defaultValue: Boolean? = null, key: String): ObsPref<Boolean>() {
-	override var pref by prefNode.BoolPref(defaultValue, key)
-  }
+    protected inner class ObjObsPref<T : Any>(
+        ser: KSerializer<T>,
+        defaultValue: T? = null,
+        key: String,
+        silent: Boolean
+    ) :
+        ObsPref<T>() {
+        override var pref by prefNode.ObjPref(ser, defaultValue, key, silent = silent)
+    }
+
+    protected inner class StringObsPrefProvider(private val defaultValue: String? = null) {
+        operator fun provideDelegate(
+            thisRef: Any?,
+            prop: KProperty<*>
+        ) = StringObsPref(defaultValue = defaultValue, prop.name)
+    }
+
+    protected inner class StringObsPref(
+        defaultValue: String? = null,
+        key: String
+    ) : ObsPref<String>() {
+        override var pref by prefNode.StringPref(defaultValue, key)
+    }
+
+    protected inner class IntObsPrefProvider(private val defaultValue: Int? = null) {
+        operator fun provideDelegate(
+            thisRef: Any?,
+            prop: KProperty<*>
+        ) = IntObsPref(defaultValue, prop.name)
+    }
+
+    protected inner class IntObsPref(
+        defaultValue: Int? = null,
+        key: String
+    ) : ObsPref<Int>() {
+        override var pref by prefNode.IntPref(defaultValue, key)
+    }
+
+    protected inner class BoolObsPrefProvider(private val defaultValue: Boolean? = null) {
+        operator fun provideDelegate(
+            thisRef: Any?,
+            prop: KProperty<*>
+        ) = BoolObsPref(defaultValue, prop.name)
+    }
+
+    protected inner class BoolObsPref(
+        defaultValue: Boolean? = null,
+        key: String
+    ) : ObsPref<Boolean>() {
+        override var pref by prefNode.BoolPref(defaultValue, key)
+    }
 }
--- src/jvmMain/kotlin/matt/pref/pref.kt
+++ src/jvmMain/kotlin/matt/pref/pref.kt
@@ -12,130 +12,162 @@ import matt.pref.safepref.SafePref
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-abstract class PrefNodeBase: Deletable {
-  protected abstract fun string(defaultValue: String? = null): Any
-  protected abstract fun int(defaultValue: Int? = null): Any
-  protected abstract fun bool(defaultValue: Boolean? = null): Any
+abstract class PrefNodeBase : Deletable {
+    protected abstract fun string(defaultValue: String? = null): Any
+    protected abstract fun int(defaultValue: Int? = null): Any
+    protected abstract fun bool(defaultValue: Boolean? = null): Any
 }
 
-open class PrefNode(private val key: String, oldKeys: List<String>, val json: Json = Json): PrefNodeBase() {
+open class PrefNode(
+    private val key: String,
+    oldKeys: List<String>,
+    val json: Json = Json
+) : PrefNodeBase() {
 
 
-  private val prefs = SafePref(key)
+    private val prefs = SafePref(key)
 
-  //  private val myStr by lazy {
-  //	toStringBuilder(
-  //	  "key" to key
-  //	)
-  //  }
+    //  private val myStr by lazy {
+    //	toStringBuilder(
+    //	  "key" to key
+    //	)
+    //  }
 
-  override fun toString() = "${this::class.simpleName}[key=$key]"
+    override fun toString() = "${this::class.simpleName}[key=$key]"
 
-  override fun delete() {
-	prefs.removeNode()
-	prefs.flush()
-	println("deleted $this")
-  }
+    override fun delete() {
+        prefs.removeNode()
+        prefs.flush()
+        println("deleted $this")
+    }
 
-  init {
-	oldKeys.forEach {
-	  prefs.remove(it)
-	}
-  }
+    init {
+        oldKeys.forEach {
+            prefs.remove(it)
+        }
+    }
 
-  protected inline fun <reified T: Any> obj(defaultValue: T? = null) =
-	ObjPrefProvider(T::class.serializer(), defaultValue)
+    protected inline fun <reified T : Any> obj(defaultValue: T? = null) =
+        ObjPrefProvider(serializer<T>(), defaultValue)
 
-  override fun string(defaultValue: String?) = provider { StringPref(defaultValue, it) }
-  override fun int(defaultValue: Int?) = provider { IntPref(defaultValue, it) }
-  override fun bool(defaultValue: Boolean?) = provider { BoolPref(defaultValue, it) }
-
-
-  abstract inner class Pref<T>(val name: String? = null, protected val defaultValue: T? = null):
-	  ReadWriteProperty<Any?, T?> {
-	/*i use my own implementation of defaults because java's implementation seems confusing*/
-	override operator fun getValue(
-	  thisRef: Any?, property: KProperty<*>
-	) = if (name!! !in prefs.keys) defaultValue else getFromNode()
-
-	abstract fun getFromNode(): T?
-	abstract fun putIntoNode(t: T)
-	override operator fun setValue(
-	  thisRef: Any?, property: KProperty<*>, value: T?
-	) {
-	  if (value == null) {
-		prefs.remove(name!!).also { println("removed pref ${prefs}..${name}") }
-	  } else putIntoNode(value)
-	  prefs.flush()
-	}
-  }
-
-  inner class ObsObjPref<T: Any>(
-	private val ser: KSerializer<T>, val defaultValue: ()->T, val name: String? = null
-  ) {
-
-	fun get() = if (name!! !in prefs.keys) {
-	  defaultValue()
-	} else getFromNode()
-
-	operator fun getValue(
-	  thisRef: Any?, property: KProperty<*>
-	): T = get()
+    override fun string(defaultValue: String?) = provider { StringPref(defaultValue, it) }
+    override fun int(defaultValue: Int?) = provider { IntPref(defaultValue, it) }
+    override fun bool(defaultValue: Boolean?) = provider { BoolPref(defaultValue, it) }
 
 
-	fun getFromNode() = try {
-	  json.decodeFromString(ser, prefs.get(name, null))
-	} catch (e: SerializationException) {
-	  warn("got $e when trying to load preference node \"$name\"")
-	  e.printReport()
-	  defaultValue()
-	}
+    abstract inner class Pref<T>(
+        val name: String? = null,
+        protected val defaultValue: T? = null
+    ) :
+        ReadWriteProperty<Any?, T?> {
+        /*i use my own implementation of defaults because java's implementation seems confusing*/
+        override operator fun getValue(
+            thisRef: Any?,
+            property: KProperty<*>
+        ) = if (name!! !in prefs.keys) defaultValue else getFromNode()
 
-	fun putIntoNode(t: T, silent: Boolean = false) =
-	  prefs.put(name!!, json.encodeToString(ser, t)).also { if (!silent) println("set pref ${prefs}..${name}=${t}") }
-  }
+        abstract fun getFromNode(): T?
+        abstract fun putIntoNode(t: T)
+        override operator fun setValue(
+            thisRef: Any?,
+            property: KProperty<*>,
+            value: T?
+        ) {
+            if (value == null) {
+                prefs.remove(name!!).also { println("removed pref ${prefs}..${name}") }
+            } else putIntoNode(value)
+            prefs.flush()
+        }
+    }
 
-  inner class ObjPrefProvider<T: Any>(
-	private val ser: KSerializer<T>, private val defaultValue: T? = null,
-  ) {
-	operator fun provideDelegate(
-	  thisRef: Any?, prop: KProperty<*>
-	) = ObjPref(ser = ser, defaultValue = defaultValue, name = prop.name, silent = false)
-  }
+    inner class ObsObjPref<T : Any>(
+        private val ser: KSerializer<T>,
+        val defaultValue: () -> T,
+        val name: String? = null
+    ) {
 
-  inner class ObjPref<T: Any>(
-	private val ser: KSerializer<T>, defaultValue: T? = null, name: String? = null, val silent: Boolean
-  ): Pref<T>(name = name, defaultValue = defaultValue) {
-	override fun getFromNode() = try {
-	  json.decodeFromString(ser, prefs.get(name, null))
-	} catch (e: SerializationException) {
-	  warn("got $e when trying to load preference node \"$name\"")
-	  e.printReport()
-	  defaultValue
-	}
+        fun get() = if (name!! !in prefs.keys) {
+            defaultValue()
+        } else getFromNode()
 
-	override fun putIntoNode(t: T) =
-	  prefs.put(name!!, json.encodeToString(ser, t)).also { if (!silent) println("set pref ${prefs}..${name}=${t}") }
-  }
+        operator fun getValue(
+            thisRef: Any?,
+            property: KProperty<*>
+        ): T = get()
 
-  inner class StringPref(defaultValue: String? = null, name: String? = null):
-	  Pref<String>(name = name, defaultValue = defaultValue) {
-	override fun getFromNode(): String? = prefs.get(name, defaultValue)
-	override fun putIntoNode(t: String) = prefs.put(name!!, t)
-  }
 
-  inner class IntPref(defaultValue: Int? = null, name: String? = null):
-	  Pref<Int>(name = name, defaultValue = defaultValue) {
-	override fun getFromNode() = prefs.getInt(name, defaultValue ?: 0)
-	override fun putIntoNode(t: Int) = prefs.putInt(name!!, t)
-  }
+        fun getFromNode() = try {
+            json.decodeFromString(ser, prefs.get(name, null))
+        } catch (e: SerializationException) {
+            warn("got $e when trying to load preference node \"$name\"")
+            e.printReport()
+            defaultValue()
+        }
 
-  inner class BoolPref(defaultValue: Boolean? = null, name: String? = null):
-	  Pref<Boolean>(name = name, defaultValue = defaultValue) {
-	override fun getFromNode() = prefs.getBoolean(name, defaultValue ?: false)
-	override fun putIntoNode(t: Boolean) {
-	  prefs.putBoolean(name!!, t)
-	}
-  }
+        fun putIntoNode(
+            t: T,
+            silent: Boolean = false
+        ) =
+            prefs.put(name!!, json.encodeToString(ser, t))
+                .also { if (!silent) println("set pref ${prefs}..${name}=${t}") }
+    }
+
+    inner class ObjPrefProvider<T : Any>(
+        private val ser: KSerializer<T>,
+        private val defaultValue: T? = null,
+    ) {
+        operator fun provideDelegate(
+            thisRef: Any?,
+            prop: KProperty<*>
+        ) = ObjPref(ser = ser, defaultValue = defaultValue, name = prop.name, silent = false)
+    }
+
+    inner class ObjPref<T : Any>(
+        private val ser: KSerializer<T>,
+        defaultValue: T? = null,
+        name: String? = null,
+        val silent: Boolean
+    ) : Pref<T>(name = name, defaultValue = defaultValue) {
+        override fun getFromNode() = try {
+            json.decodeFromString(ser, prefs.get(name, null))
+        } catch (e: SerializationException) {
+            warn("got $e when trying to load preference node \"$name\"")
+            e.printReport()
+            defaultValue
+        }
+
+        override fun putIntoNode(t: T) =
+            prefs.put(name!!, json.encodeToString(ser, t))
+                .also { if (!silent) println("set pref ${prefs}..${name}=${t}") }
+    }
+
+    inner class StringPref(
+        defaultValue: String? = null,
+        name: String? = null
+    ) :
+        Pref<String>(name = name, defaultValue = defaultValue) {
+        override fun getFromNode(): String? = prefs.get(name, defaultValue)
+        override fun putIntoNode(t: String) = prefs.put(name!!, t)
+    }
+
+    inner class IntPref(
+        defaultValue: Int? = null,
+        name: String? = null
+    ) :
+        Pref<Int>(name = name, defaultValue = defaultValue) {
+        override fun getFromNode() = prefs.getInt(name, defaultValue ?: 0)
+        override fun putIntoNode(t: Int) = prefs.putInt(name!!, t)
+    }
+
+    inner class BoolPref(
+        defaultValue: Boolean? = null,
+        name: String? = null
+    ) :
+        Pref<Boolean>(name = name, defaultValue = defaultValue) {
+        override fun getFromNode() = prefs.getBoolean(name, defaultValue ?: false)
+        override fun putIntoNode(t: Boolean) {
+            prefs.putBoolean(name!!, t)
+        }
+    }
 }
 
